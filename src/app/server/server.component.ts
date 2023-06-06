1b0b059fab036314816f555f7633998777ba46df
Rebecca Costa Manzi <rebeccamanzi@MacBook-Pro-de-Rebecca.local>
2023-06-06 09:14:24 -0300

    creating a project with components

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // element to use  in the html
+  selector: 'app-server',
+  // html file
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {}
