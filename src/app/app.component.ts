7c606a6e86005f69a5a141f9bd79552118cd8811
Rebeca Manzi <rebeccamanzi@gmail.com>
2023-06-06 09:40:14 -0300

    components templates and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  //styleUrls: ['./app.component.css'],
+  // use the style in the ts file only if don't have a lot of code
+  styles: [
+    `
+      h3 {
+        color: dodgerblue;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   name = 'Rebecca';
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
-})
-export class ServersComponent {
+  // selector by tags:
+  // selector: 'app-servers',
 
-}
+  // selector by atribute:
+  // selector: '[app-servers]',
+
+  // selector by class:
+  selector: '.app-servers',
+
+  // selector by id it's not supported
+
+  // only use the template like this if you have untill 3 lines of code
+  template: `<app-server></app-server> <app-server></app-server>`,
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent {}
